a3fca8d3f5657f44154b81483c876f198c945983
vanityfer <75648982+vanityfer@users.noreply.github.com>
2021-09-01 18:40:28 -0300

    Delete app.js

--- routes/app.js
+++
@@ -1,9 +0,0 @@
-const app = express();
-app.get('/', function(req, res) {
-    respuesta = {
-        error: true,
-        codigo: 200,
-        mensaje: 'hola mundo'
-    };
-    res.send(respuesta);
-});
