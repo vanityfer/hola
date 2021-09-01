8384084a47b35dfa17eb0f87daf5aa87035b40ae
vanityfer <75648982+vanityfer@users.noreply.github.com>
2021-09-01 19:07:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
+const app = express();
+app.get('/', function(req, res) {
+    respuesta = {
+        error: true,
+        codigo: 200,
+        mensaje: 'hola mundo'
+    };
+    res.send(respuesta);
+});
+
 const express = require("express");
 
 const app = express();
